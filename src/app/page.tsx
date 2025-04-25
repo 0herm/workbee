0fe9dd335f7d4309e5b42d432ecad4da10db8147
0herm <0herm@protonmail.com>
2025-04-25 20:58:21 +0200

    landing page lang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import { SiGithub, SiLinkedin, SiGitlab } from 'react-icons/si'
+import no from '@text/landing/no.json'
+import en from '@text/landing/en.json'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Home() {
+    const lang = (await cookies()).get('lang')?.value || 'no'
+    const text = lang === 'no' ? no : en
+
     return (
         <div className='p-[4rem]'>
             <div className='flex flex-col gap-[1rem]'>
-                <h1 className='text-4xl font-bold max-w-[30rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-                <p  className='font-light text-almostbright max-w-[40rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec placerat nisl. Mauris a suscipit eros. Nam ut dapibus ex. Nulla tempor ullamcorper sem a ultricies. Nam vel orci a dui placerat aliquet vitae id erat. Aenean nec gravida odio. Quisque finibus ex viverra, tincidunt arcu sed, tempus lorem. Maecenas sed orci dolor.</p>
+                <h1 className='text-4xl font-bold max-w-[30rem]'>{text.title}</h1>
+                <p  className='font-light text-almostbright max-w-[40rem]'>{text.description}</p>
                 <div className='flex flex-row gap-[0.75rem]'>
                     <Link href={'Dummy'} className='group'>
                         <SiGithub className='fill-almostbright size-[1.25rem] transition-colors duration-300 group-hover:fill-white' />
@@ -20,7 +26,38 @@ export default function Home() {
                 </div>
             </div>
             <div>
-
+                <div>
+                    <div>
+                        {text.education.title}
+                    </div>
+                    <ol>
+                        {Object.values(text.education.degrees).map((item, index) => (
+                            <li key={index}>
+                                <span className=''>{item.title}</span>
+                                <div>
+                                    <span>{item.degree}</span>
+                                    <span>{item.period}</span>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+                {/* <div>
+                    <div>
+                        {text.work.title}
+                    </div>
+                    <ol>
+                        {text.work.items.map((item, index) => (
+                            <li key={index}>
+                                <span className=''>{item.position}</span>
+                                <div>
+                                    <span>{item.company}</span>
+                                    <span>{item.period}</span>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+                </div> */}
             </div>
         </div>
     )
